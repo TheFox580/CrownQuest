65156292af9b00b17792d2b14491a784477313f1
Fox <zoelliotmitong@gmail.com>
2024-01-09 00:27:50 +0100

    Added no boat collision for future update

--- src/main/java/fr/thefox580/crownquest/listeners/onNoxesiumJoinEvent.java
+++ src/main/java/fr/thefox580/crownquest/listeners/onNoxesiumJoinEvent.java
@@ -17,13 +17,12 @@ public class onNoxesiumJoinEvent implements Listener {
         Player player = event.getPlayer();
 
         var noTridentCollision = NoxesiumUtils.getManager().<Boolean>getServerRule(player, ServerRuleIndices.DISABLE_SPIN_ATTACK_COLLISIONS);
-        //var noBoatCollision = NoxesiumUtils.getManager().<Boolean>getServerRule(player, ServerRuleIndices.DISABLE_BOAT_COLLISIONS);
-
         noTridentCollision.setValue(true);
-        //noBoatCollision.setValue(true);
-
         new ClientboundChangeServerRulesPacket(List.of(noTridentCollision)).send(player);
-        //new ClientboundChangeServerRulesPacket(List.of(noBoatCollision)).send(player);
+
+        var noBoatCollision = NoxesiumUtils.getManager().<Boolean>getServerRule(player, ServerRuleIndices.DISABLE_BOAT_COLLISIONS);
+        noBoatCollision.setValue(true);
+        new ClientboundChangeServerRulesPacket(List.of(noBoatCollision)).send(player);
     }
 
 }
